6e05c31045b9b5d36daaa0c4e03c3d5d9f1857b9
Park Chan Ho <qwsa1258@gmail.com>
2023-11-21 11:52:02 +0900

    game_start_packet 구현/ send_sc_game_start_packet 구현

--- Run_Server/Run_Server/Server.cpp
+++ Run_Server/Run_Server/Server.cpp
@@ -131,6 +131,30 @@ void send_sc_map_data_packet()
 	}
 }
 
+void send_sc_game_start_packet()
+{
+	SC_GAME_START_PACKET p;
+	p.size = sizeof(p);
+	p.type = SC_GAME_START;
+
+	// 전역 데이터 복사
+	g_mutex.lock();
+	std::array<SOCKET, 3>	client_sockets = g_client_sockets;
+	std::array<bool, 3>		is_accept = g_is_accept;
+	g_mutex.unlock();
+
+	// 모든 클라이언트에게 전송
+	for (int i = 0; i < 3; ++i) {
+		if (is_accept[i]) {
+			int retval = send(client_sockets[i], reinterpret_cast<char*>(&p), sizeof(p), 0);
+			if (retval == SOCKET_ERROR) {
+				err_display("send()");
+				//break;	// 차후 고민 필요....
+			}
+		}
+	}
+}
+
 void send_sc_position_packet()
 {
 	SC_POSITION_PACKET p;
@@ -184,6 +208,7 @@ void send_sc_game_end_packet()
 		}
 	}
 }
+
 // 패킷 처리하는 함수
 void process_packet(int my_id, char* packet)
 {
